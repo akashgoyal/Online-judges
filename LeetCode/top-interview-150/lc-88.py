8e9570a29de56431a41ea57246fcb3c77a2c7f1f
Akash <akashgoyal7@gmail.com>
2024-07-06 20:34:47 +0530

    [INIT] lc initial push

---
+++ LeetCode/top-interview-150/lc-88.py
@@ -0,0 +1,18 @@
+# 88. Merge Sorted Array
+
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j, ptr = m-1, n-1, m+n-1
+
+        while j>=0:
+            if i>=0 and nums1[i]>nums2[j]:
+                nums1[ptr] = nums1[i]
+                i -= 1
+            else:
+                nums1[ptr] = nums2[j]
+                j -= 1
+            ptr -=1
+        
+        return
